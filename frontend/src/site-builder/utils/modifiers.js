77c9975f5118c705129460fda7b152bbc13e1e96
Irfan Sindhi <irfansindhi2@gmail.com>
2024-09-27 14:36:26 -0500

    removed error coming from typescript

--- frontend/src/site-builder/utils/modifiers.js
+++ frontend/src/site-builder/utils/modifiers.js
@@ -1,7 +1,7 @@
 import { Modifier } from '@dnd-kit/core';
 
 export const snapToGrid = (gridSize) => {
-  const modifier: Modifier = ({ transform }) => {
+  const modifier= ({ transform }) => {
     return {
       x: Math.round(transform.x / gridSize) * gridSize,
       y: Math.round(transform.y / gridSize) * gridSize,
